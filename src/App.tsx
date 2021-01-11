1d86e7df49cb12eed9eab0e47258ac331eae6556
Pulak Kumar Das <pulak1901@gmail.com>
2021-01-11 21:13:17 +0530

    Remove default HTML/CSS and test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { useDispatch } from 'react-redux'
 import { Dispatch } from 'redux'
@@ -23,21 +22,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
       <input type="file" onChange={onFileSelected} accept="audio/*"/>
     </div>
   );

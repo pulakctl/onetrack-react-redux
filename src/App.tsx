457e345500bb56fab54160c33b381cf8084b539b
Pulak Kumar Das <pulak1901@gmail.com>
2021-01-11 21:09:53 +0530

    Update App component to include file input element

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch } from 'react-redux'
+import { Dispatch } from 'redux'
+import { loadFile } from './store/actionCreators';
+
+import * as mm from 'music-metadata-browser'
 
 function App() {
+  const dispatch: Dispatch<any> = useDispatch()
+
+  const onFileSelected = React.useCallback(
+    (event) => {
+      const file = event.target.files[0]
+      mm.parseBlob(file).then((metadata) => {
+        const title = (metadata.common.title) ? metadata.common.title : ""
+        dispatch(loadFile(file.name, title))
+      })
+    },
+    [dispatch]
+  )
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +37,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <input type="file" onChange={onFileSelected} accept="audio/*"/>
     </div>
   );
 }

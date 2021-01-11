717c133819dba7bbedeaa684585ff974416a7929
Pulak Kumar Das <pulak1901@gmail.com>
2021-01-11 23:26:26 +0530

    Update App.tsx, actionCreators.ts, and reducer.ts to show album art

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,8 +1,10 @@
 import * as actionTypes from "./actionTypes"
+import blank_album_art from "../assets/blank_album.svg"
 
 const initialState: PlayerState = {
     title: '',
-    fileName: ''
+    fileName: '',
+    albumArt: blank_album_art
 }
 
 const reducer = (
@@ -13,7 +15,8 @@ const reducer = (
         case actionTypes.LOAD_FILE:
             return {
                 title: action.title,
-                fileName: action.fileName
+                fileName: action.fileName,
+                albumArt: action.albumArt
             }
     }
 

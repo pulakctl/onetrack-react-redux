72b71c201fd6d33dde60505f13e8859c3c84aa02
Pulak Kumar Das <pulak1901@gmail.com>
2021-01-16 00:20:22 +0530

    Select Vibrant and DarkMuted as the two chosen colors

--- src/components/OpenButton.tsx
+++ src/components/OpenButton.tsx
@@ -39,8 +39,24 @@ export const OpenButton: FC<Props> = (props: Props) => {
 
                     Vibrant.from(art).getPalette(
                         (err, palette) => {
-                            if (palette?.LightVibrant !== null && palette?.DarkMuted !== null) {
-                                let a = palette?.LightVibrant?.rgb as Vec3
+                            /*
+                            let c = palette?.DarkMuted?.rgb as Vec3
+                            console.log('%c Dark Muted', `color: rgb(${c[0]}, ${c[1]}, ${c[2]})`)
+                            c = palette?.Muted?.rgb as Vec3
+                            console.log('%c Muted', `color: rgb(${c[0]}, ${c[1]}, ${c[2]})`)
+                            c = palette?.LightMuted?.rgb as Vec3
+                            console.log('%c Light Muted', `color: rgb(${c[0]}, ${c[1]}, ${c[2]})`)
+
+                            c = palette?.DarkVibrant?.rgb as Vec3
+                            console.log('%c Dark Vibrant', `color: rgb(${c[0]}, ${c[1]}, ${c[2]})`)
+                            c = palette?.Vibrant?.rgb as Vec3
+                            console.log('%c Vibrant', `color: rgb(${c[0]}, ${c[1]}, ${c[2]})`)
+                            c = palette?.LightVibrant?.rgb as Vec3
+                            console.log('%c Light Vibrant', `color: rgb(${c[0]}, ${c[1]}, ${c[2]})`)
+                            */
+
+                            if (palette?.Vibrant !== null && palette?.DarkMuted !== null) {
+                                let a = palette?.Vibrant?.rgb as Vec3
                                 let b = palette?.DarkMuted?.rgb as Vec3
                                 let colors = [a, b]
 

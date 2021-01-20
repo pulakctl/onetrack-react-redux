04eca4d74efdf1df3d677edbed8796b2a57e658d
Pulak Kumar Das <pulak1901@gmail.com>
2021-01-20 20:32:00 +0530

    Add basic service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import { createStore, applyMiddleware, Store } from 'redux'
 import { Provider } from 'react-redux'
@@ -26,4 +27,6 @@ render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
+
+serviceWorkerRegistration.register()

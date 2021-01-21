5ed5ee240a19ad2cb48ea0e1bfa8be79110986ea
Pulak Kumar Das <pulak1901@gmail.com>
2021-01-21 13:31:46 +0530

    Update TrackDetailsDisplay to display all metadata

--- src/components/TrackDetailsDisplay.tsx
+++ src/components/TrackDetailsDisplay.tsx
@@ -13,10 +13,41 @@ type Props = {
 }
 
 const TrackDetailsDisplay = (props: Props) => {
+    let title = 'N/A', trackArtists = 'N/A', album = 'N/A', albumArtists = 'N/A'
+    if (props.metadata.title !== '') {
+        title = props.metadata.title
+    }
+    if (props.metadata.trackArtists !== '') {
+        trackArtists = props.metadata.trackArtists
+    }
+    if (props.metadata.album !== '') {
+        album = props.metadata.album
+    }
+    if (props.metadata.albumArtists !== '') {
+        albumArtists = props.metadata.albumArtists
+    }
+    
     return (
         <div id={props.id}>
             <div className="track-detail">
-                    <p className="track-detail-content">{props.fileName}</p>
+                <div className="track-detail-label">FILENAME</div>
+                <p className="track-detail-content">{props.fileName}</p>
+            </div>
+            <div className="track-detail">
+                <div className="track-detail-label">TITLE</div>
+                <p className="track-detail-content">{title}</p>
+            </div>
+            <div className="track-detail">
+                <div className="track-detail-label">BY</div>
+                <p className="track-detail-content">{trackArtists}</p>
+            </div>
+            <div className="track-detail">
+                <div className="track-detail-label">ALBUM</div>
+                <p className="track-detail-content">{album}</p>
+            </div>
+            <div className="track-detail">
+                <div className="track-detail-label">BY</div>
+                <p className="track-detail-content">{albumArtists}</p>
             </div>
         </div>
     )
